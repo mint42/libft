076589fad0594f0e4fded3c78c012e5fee60440c
Caelan Reedy <rreedy@e1z3r7p3.42.us.org>
2018-08-18 11:46:39 -0700

    stresize

--- ft_stresize.c
+++ ft_stresize.c
@@ -6,13 +6,13 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/18 11:38:53 by rreedy            #+#    #+#             */
-/*   Updated: 2018/08/18 11:39:26 by rreedy           ###   ########.fr       */
+/*   Updated: 2018/08/18 11:45:37 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-char	*ft_stresize(char **s, int in, int size)
+char	*ft_stresize(char **s, int in, size_t size)
 {
 	char	*str;
 
