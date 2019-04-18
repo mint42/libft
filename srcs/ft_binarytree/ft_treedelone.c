43d4310821837b50a457dbe9715ced47c2fc5a29
Ari <arireedy42@gmail.com>
2019-04-17 18:43:58 -0700

    del function takes a single pointer oops

--- srcs/ft_binarytree/ft_treedelone.c
+++ srcs/ft_binarytree/ft_treedelone.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 12:54:07 by rreedy            #+#    #+#             */
-/*   Updated: 2019/04/16 01:49:15 by rreedy           ###   ########.fr       */
+/*   Updated: 2019/04/17 18:43:19 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_treedelone(t_binarytree **node, void (*del)(void *, size_t))
 {
 	if (*node)
 	{
-		del(&(*node)->content, (*node)->content_size);
+		del((*node)->content, (*node)->content_size);
 		ft_memdel((void **)node);
 	}
 }
