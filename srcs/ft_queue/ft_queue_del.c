9afe8ce2c4ec7898defb412dbd2bf15bb18d060a
Ari <arireedy42@gmail.com>
2019-04-20 19:06:23 -0700

    moving variable setting until after initial checks

--- srcs/ft_queue/ft_queue_del.c
+++ srcs/ft_queue/ft_queue_del.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 12:39:17 by rreedy            #+#    #+#             */
-/*   Updated: 2019/04/20 12:53:21 by rreedy           ###   ########.fr       */
+/*   Updated: 2019/04/20 18:59:47 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@ void	ft_queue_del(t_queue **queue, void (*del)(void *))
 {
 	void	*content;
 
+	if (!(*queue))
+		return ;
 	content = 0;
-	if (*queue)
+	while (!ft_queue_is_empty(*queue))
 	{
-		while (!ft_queue_is_empty(*queue))
-		{
-			content = ft_dequeue(*queue);
-			del(content);
-		}
-		ft_memdel((void **)queue);
+		content = ft_dequeue(*queue);
+		del(content);
 	}
+	ft_memdel((void **)queue);
 }
--- srcs/ft_stack/ft_stack_del.c
+++ srcs/ft_stack/ft_stack_del.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 12:13:55 by rreedy            #+#    #+#             */
-/*   Updated: 2019/04/20 12:53:07 by rreedy           ###   ########.fr       */
+/*   Updated: 2019/04/20 18:59:12 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@ void	ft_stack_del(t_stack **stack, void (*del)(void *))
 {
 	void	*content;
 
+	if (!(*stack))
+		return ;
 	content = 0;
-	if (*stack)
+	while (!ft_stack_is_empty(*stack))
 	{
-		while (!ft_stack_is_empty(*stack))
-		{
-			content = ft_stack_pop(*stack);
-			del(content);
-		}
-		ft_memdel((void **)stack);
+		content = ft_stack_pop(*stack);
+		del(content);
 	}
+	ft_memdel((void **)stack);
 }
