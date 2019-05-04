5107e2da243265287f6884b06ec9fd1797b6051b
Ari <arireedy42@gmail.com>
2019-05-04 00:51:16 -0700

    making simpler functions

--- srcs/ft_stack/ft_stack_rrotate.c
+++ srcs/ft_stack/ft_stack_rrotate.c
@@ -6,7 +6,7 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 22:48:05 by rreedy            #+#    #+#             */
-/*   Updated: 2019/05/03 23:09:52 by rreedy           ###   ########.fr       */
+/*   Updated: 2019/05/04 00:50:41 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,14 @@
 void	ft_stack_rrotate(t_stack *stack)
 {
 	t_snode		*cur;
-	void		*content;
 
 	if (!stack || !(stack->top) || !(stack->top->next) || !(stack->bottom))
 		return ;
 	cur = stack->top;
 	while (cur->next->next)
 		cur = cur->next;
-	stack->bottom->next = stack->top->next;
-	stack->top->next = stack->bottom;
+	stack->bottom->next = stack->top;
+	stack->top = stack->bottom;
 	stack->bottom = cur;
-	stack->bottom->next = cur;
-	content = stack->top->content;
-	stack->top->content = stack->top->next->content;
-	stack->top->next->content = content;
+	stack->bottom->next = 0;
 }
