da3a4af03095c5d0b3dae5adcac65f5cc2ed6894
mint <officialscientist42@gmail.com>
2020-04-27 07:36:32 -0700

    optimizing functions

--- modules/ft_mem/srcs/ft_bzero.c
+++ modules/ft_mem/srcs/ft_bzero.c
@@ -6,14 +6,36 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 15:54:55 by rreedy            #+#    #+#             */
-/*   Updated: 2019/04/07 22:58:16 by rreedy           ###   ########.fr       */
+/*   Updated: 2020/04/27 07:32:07 by mint             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mem.h"
 #include <stddef.h>
 
-void	ft_bzero(void *s, size_t len)
+void	ft_bzero(void *ptr, size_t size)
 {
-	ft_memset(s, 0, len);
+	unsigned long int	*fast_cur;
+	unsigned long int	zero;
+	unsigned char		*slow_cur;
+	unsigned char		sizeoflong;
+
+	sizeoflong = sizeof(unsigned long int);
+	fast_cur = (unsigned long int *)ptr;
+	zero = 0;
+	while (size >= sizeoflong)
+	{
+		*fast_cur = zero;
+		++fast_cur;
+		ptr = ptr + sizeoflong;
+		size = size - sizeoflong;
+	}
+	slow_cur = (unsigned char *)fast_cur;
+	while (size > 0)
+	{
+		*slow_cur = (unsigned char)zero;
+		++slow_cur;
+		++ptr;
+		--size;
+	}
 }
--- modules/ft_mem/srcs/ft_memcpy.c
+++ modules/ft_mem/srcs/ft_memcpy.c
@@ -6,18 +6,34 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 13:30:37 by rreedy            #+#    #+#             */
-/*   Updated: 2019/04/07 22:58:16 by rreedy           ###   ########.fr       */
+/*   Updated: 2020/04/27 07:30:37 by mint             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t len)
+void	*ft_memcpy(void *dst, void *src, size_t size)
 {
-	unsigned char	*cur;
+	unsigned long int	*fast_cur;
+	unsigned char		*slow_cur;
+	unsigned char		sizeoflong;
 
-	cur = (unsigned char *)dst;
-	while (len--)
-		*cur++ = *(unsigned char *)src++;
+	sizeoflong = sizeof(unsigned long int);
+	fast_cur = (unsigned long int *)dst;
+	while (size >= sizeoflong)
+	{
+		*fast_cur = *(unsigned long int *)src;
+		++fast_cur;
+		src = src + sizeoflong;
+		size = size - sizeoflong;
+	}
+	slow_cur = (unsigned char *)fast_cur;
+	while (size > 0)
+	{
+		*slow_cur = *(unsigned char *)src;
+		++slow_cur;
+		++src;
+		--size;
+	}
 	return (dst);
 }
