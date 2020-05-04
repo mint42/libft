ef2f8bc2e79a5fae92d394ec5c1853610c6b3fd8
mint <officialscientist42@gmail.com>
2020-05-04 03:49:28 -0700

    removing unneeded include from bzero

--- modules/ft_mem/srcs/ft_bzero.c
+++ modules/ft_mem/srcs/ft_bzero.c
@@ -6,11 +6,10 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 15:54:55 by rreedy            #+#    #+#             */
-/*   Updated: 2020/04/27 07:32:07 by mint             ###   ########.fr       */
+/*   Updated: 2020/05/04 03:48:13 by mint             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_mem.h"
 #include <stddef.h>
 
 void	ft_bzero(void *ptr, size_t size)
